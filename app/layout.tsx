b42db97ce82d06d1ada79a6b772f57aa637e07a5
shibuya <illusi0n.acts.like.magic14@gmail.com>
2022-12-17 13:14:46 +0900

    react-query 追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,21 @@
 
 import { ReactNode } from 'react'
 import { SessionProvider } from 'next-auth/react'
+import { queryClient } from '../lib/query'
 import '../styles/globals.css'
 import AuthLayout from './layouts/AuthLayout'
+import { QueryClientProvider } from 'react-query'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <body>
         <SessionProvider>
-          <div className="min-h-screen bg-gray-800 text-gray-400 text-sm font-mono">
-            <AuthLayout>{children}</AuthLayout>
-          </div>
+          <QueryClientProvider client={queryClient}>
+            <div className="min-h-screen bg-gray-800 text-gray-400 text-sm font-mono">
+              <AuthLayout>{children}</AuthLayout>
+            </div>
+          </QueryClientProvider>
         </SessionProvider>
       </body>
     </html>
